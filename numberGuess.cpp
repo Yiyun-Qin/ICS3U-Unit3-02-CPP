91ab7421d3b90dc51d415ecb671c203f4df5159a
Yiyun Qin <yiyun.qin@mths.ca>
2022-03-22 18:15:13 +0000

    number guess

--- numberGuess.cpp
+++ numberGuess.cpp
@@ -2,29 +2,27 @@
 //
 // Created by Yiyun Qin
 // Created at March 2022
-// This is a math program with proper style
+// This is a math program, calculating the number guess game
 
 #include <iostream>
 
 int main() {
-    // This function calculates the cost of a pizza
+    // This function comparing the guessed number and answer
     const int ANSWER = 6;
     int numberGuess;
 
     // input
     std::cout << "Enter the number you guess between 0 - 9: ";
-    std::cin >> diameter;
+    std::cin >> numberGuess;
 
     // process
-    untaxedPrice = LABOR + RENT + MATERIAL_PER_INCH * diameter;
-    HST = untaxedPrice * 0.13;
-    totalCost = untaxedPrice + HST;
-
-    // output
     std::cout << std::endl;
-    std::cout << "The cost of a " << diameter <<
-    " inch pizza is $" << std::fixed
-    << std::setprecision(2) << std::setfill('0') << totalCost << std::endl;
-
-    std::cout << "\nDone." << std::endl;
+    if (numberGuess == ANSWER) {
+        // output
+        std::cout << "Your guess is right!";
+    }
+    if (numberGuess != ANSWER) {
+        // output
+        std::cout << "Your guess is wrong!";
+    }
 }
